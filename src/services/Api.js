7a14e5e4e713094b31be74af0520eccf85ff0240
Nefta11 <neftaliarturohernandez@gmail.com>
2025-03-04 11:55:49 -0600

    agregar servicios de API para creación y autenticación de usuarios

---
+++ src/services/Api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:8000';
+
+export const createUser = async (user) => {
+    try {
+        const response = await axios.post(`${API_URL}/users/`, user);
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response.data.detail || 'Error al crear el usuario');
+    }
+};
+
+export const loginUser = async (user) => {
+    try {
+        const response = await axios.post(`${API_URL}/login`, user);
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response.data.detail || 'Correo electrónico o contraseña incorrectos');
+    }
+};
