e3cc4092513578e141907f8138ec4df372ab933e
Nefta11 <neftaliarturohernandez@gmail.com>
2025-03-19 21:21:27 -0600

    actualizar la URL de la API para usar el entorno de producción

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://crudpythonmysql.onrender.com';
 
 export const createUser = async (user) => {
     try {
